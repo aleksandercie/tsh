4f2d6960942858762081d372be146cdacd27129f
Aleksander Gasior <aleksandergasior@MacBook-Air-Aleksander.local>
2022-01-14 13:42:35 +0100

    Updates

---
+++ precache-manifest.e2a023e251ef9fc49ddf64d51b87fae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7378b25b92feef1600d12953dec899a2",
+    "url": "/tsh/index.html"
+  },
+  {
+    "revision": "8c0bb37fdd62fa2703a6",
+    "url": "/tsh/static/css/main.72fb0be9.chunk.css"
+  },
+  {
+    "revision": "6729b6cd7ccefc34ea1e",
+    "url": "/tsh/static/js/2.37d8263f.chunk.js"
+  },
+  {
+    "revision": "6ef57ea73f859e79d7f696b27898a6e5",
+    "url": "/tsh/static/js/2.37d8263f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c0bb37fdd62fa2703a6",
+    "url": "/tsh/static/js/main.0af9d373.chunk.js"
+  },
+  {
+    "revision": "50b3e408c7ddeb361cbf",
+    "url": "/tsh/static/js/runtime-main.5be3a166.js"
+  }
+]);
