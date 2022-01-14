6a510ac192adb1a1b50bbe281a3d0d253e89e697
Aleksander Gasior <aleksandergasior@MacBook-Air-Aleksander.local>
2022-01-14 13:54:11 +0100

    fix button color

---
+++ build/precache-manifest.2b8c2f113c45e8f49f363fd414b6f681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eacf0b99c9a35c4f0ff0d0f3c20534c9",
+    "url": "/tsh/index.html"
+  },
+  {
+    "revision": "8ae0c61c75bb4b792822",
+    "url": "/tsh/static/css/main.ebd276a8.chunk.css"
+  },
+  {
+    "revision": "6729b6cd7ccefc34ea1e",
+    "url": "/tsh/static/js/2.37d8263f.chunk.js"
+  },
+  {
+    "revision": "6ef57ea73f859e79d7f696b27898a6e5",
+    "url": "/tsh/static/js/2.37d8263f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae0c61c75bb4b792822",
+    "url": "/tsh/static/js/main.0af9d373.chunk.js"
+  },
+  {
+    "revision": "50b3e408c7ddeb361cbf",
+    "url": "/tsh/static/js/runtime-main.5be3a166.js"
+  }
+]);
